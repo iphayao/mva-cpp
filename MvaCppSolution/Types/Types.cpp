fb3b0cbb65cc221608a75844d89fd1aae963c4a6
Phayao Boonon <phayaob@gmail.com>
2017-08-14 21:16:23 +0700

    Add static cast and auto keyword

--- MvaCppSolution/Types/Types.cpp
+++ MvaCppSolution/Types/Types.cpp
@@ -4,24 +4,24 @@
 #include <iostream>
 
 int main() {
-
-	int i{ 2 };				// i: 2
-	i = 3.2;				// i: 3
-	i = 2.9;				// i: 2
-	i = -1;					// i: -1
+	// use 'auto' keyword for auto type
+	auto i{ 2 };					// i: 2
+	i = static_cast<int>(3.2);		// i: 3
+	i = static_cast<int>(2.9);		// i: 2
+	i = -1;							// i: -1
 	std::cout << i << std::endl;
 
-	unsigned int u{ 0 };	//u: 0
-	u = -2;					//u: 4294967294
+	unsigned int u{ 0 };			//u: 0
+	u = -2;							//u: 4294967294
 
-	double d{ 2.7 };		//d: 2.7
-	i = d;					//i: 2
-	d = i;					//d: 2.000000
+	double d{ 2.7 };				//d: 2.7
+	i = static_cast<int>(d);		//i: 2
+	d = i;							//d: 2.000000
 	std::cout << d << std::endl;
 
-	bool flag{ true };		//flag: true
-	flag = false;			//flag: false
-	flag = 7;				//flag: true
+	bool flag{ true };				//flag: true
+	flag = false;					//flag: false
+	flag = static_cast<bool>(7);	//flag: true
 	std::cout << flag << std::endl;
 
 	return 0;
