10cb384f93000e6c657af80c39ea9f826690dee4
Phayao Boonon <phayaob@gmail.com>
2017-08-14 21:10:56 +0700

    Add Types project to demonstrate for type in C++

---
+++ MvaCppSolution/Types/Types.cpp
@@ -0,0 +1,28 @@
+// Types.cpp : Defines the entry proint for the console application
+// MSDN's Fundamatal Types (C++): https://msdn.microsoft.com/en-us/library/cc953fe1.aspx
+
+#include <iostream>
+
+int main() {
+
+	int i{ 2 };				// i: 2
+	i = 3.2;				// i: 3
+	i = 2.9;				// i: 2
+	i = -1;					// i: -1
+	std::cout << i << std::endl;
+
+	unsigned int u{ 0 };	//u: 0
+	u = -2;					//u: 4294967294
+
+	double d{ 2.7 };		//d: 2.7
+	i = d;					//i: 2
+	d = i;					//d: 2.000000
+	std::cout << d << std::endl;
+
+	bool flag{ true };		//flag: true
+	flag = false;			//flag: false
+	flag = 7;				//flag: true
+	std::cout << flag << std::endl;
+
+	return 0;
+}
