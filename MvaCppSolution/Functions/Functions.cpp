f91e9e4d98582bda23e8af474ebd3541d25c0890
Phayao Boonon <phayaob@gmail.com>
2017-08-15 22:41:30 +0700

    Demonstrate to use cont keyword

--- MvaCppSolution/Functions/Functions.cpp
+++ MvaCppSolution/Functions/Functions.cpp
@@ -4,11 +4,16 @@
 
 int main() {
 	int i{ 4 };
-	int j{ AddTwo(i) };
+	const int j{ AddTwo(i) };
+	int const k{ AddTwo(i) };
 
 	AddTwo(j);
 
-	j = AddTwo(j);
+	//j = AddTwo(j);	// const cannot be change
+	i = AddTwo(j);
+
+	//k = 7;			// const cannot be change
+	i = 7;
 	
 	return 0;
 }
