93cecc88accdc15485ecfe1bd4b36211e3b7de08
Phayao Boonon <phayaob@gmail.com>
2017-08-15 22:52:53 +0700

    Demonstrate to use namespace

--- MvaCppSolution/String/String.cpp
+++ MvaCppSolution/String/String.cpp
@@ -3,20 +3,22 @@
 #include <string>
 #include <iostream>
 
+using namespace std;
+
 int main() {
 	int i{ 2 };
 	
-	std::string Hello{ "Hello" };
+	string Hello{ "Hello" };
 	
 	Hello += " World!";
 	
-	std::cout << Hello << std::endl;
+	cout << Hello << endl;
 	
 	if (Hello == "Hello World!") {
-		std::cout << "equal" << std::endl;
+		cout << "equal" << endl;
 	}
 	else {
-		std::cout << "not equal" << std::endl;
+		cout << "not equal" << endl;
 	}
 
 	return 0;
