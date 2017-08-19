aa58af47f071ea78bf08fc169b75bf300e5dd353
Phayao Boonon <phayaob@gmail.com>
2017-08-19 21:16:15 +0700

    Demonstrate to use Exceptoins in C++

--- MvaCppSolution/Heap/Heap.cpp
+++ MvaCppSolution/Heap/Heap.cpp
@@ -1,9 +1,18 @@
 // Heap.cpp : Defines the entry proint for the console application
 
+#include <iostream>
+
 int main() {
-	int* p{ new int{ 1 } };
-	*p = 2;
-	delete p;
+
+	try {
+		int* p{ new int{ 1 } };
+		*p = 2;
+		throw std::exception{ "Trouble" };
+		delete p;
+	}
+	catch (const std::exception& e) {
+		std::cout << "Exception caught: " << e.what();
+	}
 
 	return 0;
 }
